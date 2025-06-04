0509d45e6538225b1c33c42de869fc6d07b858ab
Quang Thành <thanh.dangquang28@gmail.com>
2025-06-04 15:37:15 +0700

    Custom metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,27 +13,6 @@ export const metadata: Metadata = {
   title: 'Aiden Dang | Portfolio',
   description:
     'Explore my creative journey, skills, and accomplishments! Here, you’ll find highlights of my work, passion projects, and the expertise I bring to every opportunity.',
-  openGraph: {
-    images: [
-      {
-        url: 'https://www.aiden-portfolio.site/opengraph-image.png',
-        width: 1200,
-        height: 630,
-        alt: 'Aiden Dang Portfolio',
-      },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    images: [
-      {
-        url: 'https://www.aiden-portfolio.site/twitter-image.png',
-        width: 600,
-        height: 314,
-        alt: 'Aiden Dang Portfolio',
-      },
-    ],
-  },
 }
 
 export default function RootLayout({
